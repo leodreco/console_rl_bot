db881ef17c089ea0acd8445d8d188d3b3896620b
Leonardo Alegre <leonardo.alegre.c@gmail.com>
2021-01-10 08:37:06 -0500

    Red font

--- src/Console.js
+++ src/Console.js
@@ -1,13 +1,20 @@
 const moment = require('moment');
-const green = '\x1b[32m';
+
 const reset = '\x1b[0m';
+const red = '\x1b[31m';
+const green = '\x1b[32m';
 
 class Console{
     static green(str){
         console.log(green + str + reset);
     }
+
     static log(...str){
         console.log(green + moment().format('HH:mm:ss'), reset, ...str);
+    }
+
+    static error(...str){
+        console.log(red + moment().format('HH:mm:ss'), reset, ...str);
     }
 }
 
