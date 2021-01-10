0404b30324e28153eca256c6fbf78ca54bfec85e
Leonardo Alegre <leonardo.alegre.c@gmail.com>
2021-01-10 08:26:48 -0500

    Error color red

--- src/errors.js
+++ src/errors.js
@@ -1,3 +1,6 @@
+const Console = require('./Console');
+const { TimeoutError } = require('puppeteer').errors;
+
 class MissingEnvError extends Error{
     constructor(message){
         super(message);
@@ -12,9 +15,22 @@ class BadCredentialsError extends Error{
     }
 }
 
-
+function errorManager(ex){
+    if(ex instanceof MissingEnvError){
+        Console.error('Credenciales indefinidas y/o archivo .env inexistente');
+        process.exit();
+    }else if(ex instanceof BadCredentialsError){
+        Console.error('Credenciales incorrectas\nCambialas en el archivo .env');
+        process.exit();
+    }else if(ex instanceof TimeoutError){
+        Console.error('Tiempo de respuesta exedido');
+    }else{
+        Console.error('Error desconocido', ex);
+    }
+}
 
 module.exports = {
     MissingEnvError,
-    BadCredentialsError
+    BadCredentialsError,
+    errorManager
 };
