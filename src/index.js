3f3aaafad891b71eee488753715a583680366e29
Leonardo Alegre <leonardo.alegre.c@gmail.com>
2021-01-10 08:37:38 -0500

    errorManager

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const Console = require('./Console');
-const { TimeoutError } = require('puppeteer').errors;
-const { MissingEnvError, BadCredentialsError } = require('./errors');
+const { errorManager } = require('./errors');
 Console.green(`    ___  __     ___  ____  ______\n   / _ \\/ /    / _ )/ __ \\/_  __/\n  /   _/ /__  / _  / /_/ / / /   \n /_/|_/____/ /____/\\____/ /_/${process.env.npm_package_version}`);
 Console.green('\n      A bot for rl garage\n');
 
@@ -11,19 +10,10 @@ async function main(){
     try{
         let usuario = await browserManager.login(process.env.EMAIL, process.env.PASSWORD);
         Console.log(`Logueado como ${usuario}`);
-        require('./jobs')();
     }catch(ex){
-        if(ex instanceof MissingEnvError){
-            Console.log('Credenciales indefinidas y/o archivo .env inexistente');
-        }else if(ex instanceof BadCredentialsError){
-            Console.log('Credenciales incorrectas\nCambialas en el archivo .env');
-        }else if(ex instanceof TimeoutError){
-            Console.log('Tiempo de respuesta exedido');
-        }else{
-            Console.log('Error desconocido', ex)
-        }
-        process.exit();
+        errorManager(ex);
     }
+    require('./jobs')();
 }
 
 main();
